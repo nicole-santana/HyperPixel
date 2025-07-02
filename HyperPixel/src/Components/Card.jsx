63135a7397105d4f5d7e75c5d89a4c806a440fe2
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-02 16:02:49 -0300

    importação das fontes que serão usadas no projeto

--- HyperPixel/src/Components/Card.jsx
+++ HyperPixel/src/Components/Card.jsx
@@ -2,7 +2,7 @@
 
 export default function Card() {
   return (
-    <div style={{ border: '2px solid red', padding: '1rem' }}>
+    <div style={{ border: '2px solid red', padding: '1rem', fontfamily: 'Tech', }}>
       <h2>Eu sou o Card!</h2>
     </div>
   )
