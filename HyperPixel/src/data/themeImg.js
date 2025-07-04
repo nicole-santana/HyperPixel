40cdde3f7027f2472fd394aa95c7a2df0e0a8149
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-04 19:58:31 -0300

    criação do arquivo de "variáveis de imagem", finalização de todas as variáveis

---
+++ HyperPixel/src/data/themeImg.js
@@ -0,0 +1,37 @@
+const imgThemes = {
+    vermelho: {
+  
+        imgCaveira: 'src\assets\img\home\caveira-home\caveira - vermelho.png',
+        imgCorvo: "src\assets\img\home\corvo-home\corvo-vermelho.png",
+        floresSb:  "src\assets\img\home\sb-rosas-home\Rosas-vermelho.png",
+        estrelaDDevlogs: "src\assets\img\devlogs\estrelas-direita\estrelas direitas-vermelho.png",
+        estrelaEDevlogs: "src\assets\img\devlogs\estrelas-esquerda\estrelas esquerdas-vermelho.png",
+        estrelaMaisDevlogs:"src\assets\img\devlogs\estrelas\estrelas-vermelho.png",
+        figArvore: "src\assets\img\about-me\floresta-vermelho.png",
+        figHamster: "src\assets\img\other-projects\hamster-vermelho.png",
+    },
+    windows:{
+        
+        imgCaveira: 'src\assets\img\home\caveira-home\caveira - windows.png',
+        imgCorvo: "src\assets\img\home\corvo-home\corvo-windows.png",
+        floresSb:  "src\assets\img\home\sb-rosas-home\Rosas-windows.png",
+        estrelaDDevlogs: "src\assets\img\devlogs\estrelas-direita\estrelas direitas-windows.png",
+        estrelaEDevlogs: "src\assets\img\devlogs\estrelas-esquerda\estrelas esquerdas-windows.png",
+        estrelaMaisDevlogs:"src\assets\img\devlogs\estrelas\estrelas-windows.png",
+        figArvore: "src\assets\img\about-me\floresta-windows.png",
+        figHamster: "src\assets\img\other-projects\hamster-windows.png",
+    },
+    rec:{
+
+        imgCaveira: 'src\assets\img\home\caveira-home\caveira - r&c.png',
+        imgCorvo: "src\assets\img\home\corvo-home\corvo-r&c.png",
+        floresSb:  "src\assets\img\home\sb-rosas-home\Rosas-r&c.png",
+        estrelaDDevlogs: "src\assets\img\devlogs\estrelas-direita\estrelas direitas-r&c.png",
+        estrelaEDevlogs: "src\assets\img\devlogs\estrelas-esquerda\estrelas esquerdas-r&c.png",
+        estrelaMaisDevlogs: "src\assets\img\devlogs\estrelas\estrelas-r&c.png",
+        figArvore: "src\assets\img\about-me\floresta-r&c.png",
+        figHamster: "src\assets\img\other-projects\hamster-r&c.png",
+    }
+}
+
+export default imgThemes
