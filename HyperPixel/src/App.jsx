a180526b196144df1d285a8a859067c4cbfdc2dc
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-06 18:17:53 -0300

    tentando fazer o card funcionar

--- HyperPixel/src/App.jsx
+++ HyperPixel/src/App.jsx
@@ -1,9 +1,10 @@
 import Card from "./Components/Card";
+import img from "/src/assets/img/home/caveira-home/caveira-vermelho.png"
 
 export default function App(){
     return(
         <div>
-            <Card />
+            <Card width={400} height={200} src={img} />
         </div>
     )
 }
--- HyperPixel/src/Components/Card.jsx
+++ HyperPixel/src/Components/Card.jsx
@@ -1,10 +1,37 @@
 
 
-export default function Card() {
+export default function Card({width, height, src}) {
+
+// width = w = largura do card
+// height = h = altura do card
+// height menor = hm = altura da parte menor
+// height maior = hMa = altura da parte maior
+
+
+// grossura dos lados das partes menores = lm
+// grossura dos lados da parte maior = lMa
+
+// margin bottom da parte superior = mB
+// margin top da parte superior = mT
+
+  const hm = height / 14
+  const hMa = width - hm
+  const lMa = width / 58
+  const mT = height / 62.5
+  const mB = mT / 2
+  const lm = lMa / 2
+
   return (
-    <div style={{ border: '2px solid red', padding: '1rem', fontfamily: 'Tech', }}>
-      <h2>Eu sou o Card!</h2>
+    <div style={{width, height}}>
+      <div style={{ height: hm, borderWidth: lm, marginBottom: mB}}>
+        <span>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
+      </div>
+      <a  style={{ height: hMa, borderWidth: lMa, marginTop: mT, border: red}}>
+        <img src={src} alt=""/>
+      </a>
     </div>
+
+    
   )
 }
 
