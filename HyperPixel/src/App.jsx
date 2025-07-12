ef3cd0f432ebb40eba024f67a0bf0291c18a16ab
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-12 18:26:04 -0300

    componente do footer concluído

--- HyperPixel/src/App.jsx
+++ HyperPixel/src/App.jsx
@@ -1,5 +1,6 @@
 import Card from "./Components/Card";
 import Header from "./Components/Header";
+import Footer from './Components/Footer';
 import img from "/src/assets/img/home/sb-rosas-home/Rosas-windows.png";
 
 export default function App(){
@@ -7,6 +8,10 @@ export default function App(){
         <div>
             <Header/>
             <Card width={30} height={30} src={img} />
+            <Card width={30} height={30} src={img} />
+            <Card width={30} height={30} src={img} />
+            <Card width={30} height={30} src={img} />
+            <Footer/>
             
             
         </div>
---
+++ HyperPixel/src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer(){
+  return (
+    <div className="Footer">
+      <div className="Lanhouse">
+        <div className="Lanhouse-content">
+          <span>
+            HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;
+          </span>
+          <span>
+            HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;HyperPixel&#9733;
+          </span>
+        </div>
+      </div>
+
+      <div className="FooterInfo">
+        <p>Say something (or don't) at: blabla@gmail.com</p>
+        <p>HP2025</p>
+      </div>
+    </div>
+  )
+}
