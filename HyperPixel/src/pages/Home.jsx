9efa30048cbaf8a86e4e762e1ddb3e513fe04170
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-15 21:42:01 -0300

    cards do home

--- HyperPixel/src/pages/Home.jsx
+++ HyperPixel/src/pages/Home.jsx
@@ -1,12 +1,37 @@
+import './CSS/main.css';
 import Card from "../Components/Card";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import img from "../assets/img/home/corvo-home/corvo-windows.png";
 
 export default function Home(){
     return(
         <div>
             <Header />
-           
+
+            <div className="HomeCards">
+                <div className="PrimeiroCard">
+                    <Card width={20} height={20} img={img}/>
+                    <h1>
+                        <span>Meu</span>
+                        <span>blog</span>
+                        <span>sobre</span>
+                        <span>tech</span>
+                    </h1>
+                </div>
+
+                <div className="SegundoCard">
+                    <h1>
+                        <span>e coisas</span>
+                        <span>que faço</span>
+                        <span>com ela</span>
+                    </h1>
+                    <Card width={20} height={20} img={img}/>
+                </div>
+            </div>
+            
+                
+            
             <Footer />
         </div>
         
