66289e45279a3dd74158a5298a87164e99a76dc1
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-17 21:03:49 -0300

    sistema de mudança de imagem concluído

--- HyperPixel/src/data/themeImg.js
+++ HyperPixel/src/data/themeImg.js
@@ -10,7 +10,7 @@ const imgThemes = {
     figHamster: 'src/assets/img/other-projects/hamster-vermelho.png',
   },
   windows: {
-    imgCaveira: 'src/assets/img/home/caveira-home/caveira-windows.png',
+    imgCaveira: 'src/assets/img/home/caveira-home/caveira - windows.png',
     imgCorvo: 'src/assets/img/home/corvo-home/corvo-windows.png',
     floresSb: 'src/assets/img/home/sb-rosas-home/Rosas-windows.png',
     estrelaDDevlogs: 'src/assets/img/devlogs/estrelas-direita/estrelas-direitas-windows.png',
@@ -20,7 +20,7 @@ const imgThemes = {
     figHamster: 'src/assets/img/other-projects/hamster-windows.png',
   },
   rec: {
-    imgCaveira: 'src/assets/img/home/caveira-home/caveira-r&c.png',
+    imgCaveira: 'src/assets/img/home/caveira-home/caveira - r&c.png',
     imgCorvo: 'src/assets/img/home/corvo-home/corvo-r&c.png',
     floresSb: 'src/assets/img/home/sb-rosas-home/Rosas-r&c.png',
     estrelaDDevlogs: 'src/assets/img/devlogs/estrelas-direita/estrelas-direitas-r&c.png',
