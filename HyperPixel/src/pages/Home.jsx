3e661c1887fc4c2eff76091470a9d356ed5dc95b
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-20 18:19:31 -0300

    tentando consertar o overflow

--- HyperPixel/src/pages/Home.jsx
+++ HyperPixel/src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import './CSS/main.css';
 import Card from "../Components/Card";
-import Footer from "../Components/Footer";
-import Header from "../Components/Header";
 import img from "../assets/img/home/corvo-home/corvo-windows.png";
 import imgThemes from "../data/themeImg";
 
 
 export default function Home( { theme } ) {
     return(
-        <div>
+        <div className='Home'>
 
             <div className="HomeCards">
                 <div className="PrimeiroCard">
-                    <Card width={20} height={20} img={img}/>
+                    <Card width={26.8475} height={17.70625} img={img}/>
                     <h1>
                         <span>Meu</span>
                         <span>blog</span>
@@ -27,13 +25,15 @@ export default function Home( { theme } ) {
                         <span>que faço</span>
                         <span>com ela</span>
                     </h1>
-                    <Card width={20} height={20} img={img}/>
+                    <Card width={26.625} height={18.9375} img={img}/>
+                    <img  className='Corvo' src={imgThemes[theme].imgCorvo} alt="a" />
+                    <img  className='Flores' src={imgThemes[theme].floresSb} alt="a" />
                 </div>
             </div>
 
-            <div>
-                <img src={imgThemes[theme].imgCaveira} alt="a" />
-                <Card width={20} height={20} img={img}/>
+            <div className='CaveiraHome'>
+                <img  src={imgThemes[theme].imgCaveira} alt="a" />
+                <Card className="Radio" width={15.5} height={9.125} img={img}/>
             </div>
     
         </div>
