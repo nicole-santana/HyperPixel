70d4def3dbd7402db5cdab477ad3bd8b2b011a42
nicole-santana <nicole.teles.santana@gmail.com>
2025-07-30 19:46:11 -0300

    About Me completa

--- HyperPixel/src/pages/AboutMe.jsx
+++ HyperPixel/src/pages/AboutMe.jsx
@@ -1,10 +1,26 @@
 import Card from "../Components/Card";
+import img from "../assets/img/home/corvo-home/corvo-windows.png";
+import imgThemes from "../data/themeImg";
+import './CSS/main.css';
 
 
-export default function AboutMe(){
-    return(
-        <div>
-           
+export default function AboutMe({ theme }) {
+    return (
+        <div id="AboutMe">
+            <div>
+                <div id="CardAboutMe">
+                    <Card  width={47.875} height={43.125} img={img} />
+                </div>
+                
+                <img id="Arvore" src={imgThemes[theme].figArvore} alt="" />
+                <p>
+                    <span>
+                        Sem credibilidade, sem diploma,
+                    </span>
+            
+                    <span> sem foto.</span>
+                </p>
+            </div>
         </div>
-    )
+    );
 }
