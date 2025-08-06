3b2e19c354e55c9886fc83157e38084e919dc9b3
nicole-santana <nicole.teles.santana@gmail.com>
2025-08-06 16:01:51 -0300

    Devlogs completo

--- HyperPixel/src/pages/Devlogs.jsx
+++ HyperPixel/src/pages/Devlogs.jsx
@@ -3,19 +3,87 @@ import imgThemes from "../data/themeImg";
 import img from "/assets/img/home/corvo-home/corvo-windows.png";
 export default function Devlogs({ theme }){
     return(
-        <div>
+        <div id="Devlogs">
             <div id="PrimeiraParte">
-                <Card width={24.6875} height={28.8125} img={img} />
+
+                <div>
+                    <Card width={24.6875} height={28.8125} img={img} />
+                    <h2>Bla Bla project 1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi officiis excepturi quae. Numquam delectus facere ducimus veniam, architecto iste facilis laborum, quibusdam sequi ipsum minima ex sit voluptatibus deleniti?</p>
+                     <a href="" className="VerMais">&gt;&gt;Veja mais</a>
+                </div>
+                <div>
+                    <Card width={24.6875} height={28.8125} img={img} />
+                    <h2>Bla Bla project 1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi officiis excepturi quae. Numquam delectus facere ducimus veniam, architecto iste facilis laborum, quibusdam sequi ipsum minima ex sit voluptatibus deleniti?</p>
+                     <a href="" className="VerMais">&gt;&gt;Veja mais</a>
+                </div>
+                <div>
+                    <Card width={24.6875} height={28.8125} img={img} />
+                    <h2>Bla Bla project 1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi officiis excepturi quae. Numquam delectus facere ducimus veniam, architecto iste facilis laborum, quibusdam sequi ipsum minima ex sit voluptatibus deleniti?</p>
+                    
+                    <a href="" className="VerMais">&gt;&gt;Veja mais</a>
+                </div>
                 
-               <Card width={24.6875} height={28.8125} img={img} />
+                
+                
                
-                <Card width={24.6875} height={28.8125} img={img} />
             </div>
 
             <div id="Estrelas1">
                 <img id="Direita" src={imgThemes[theme].estrelaDDevlogs} alt="" />
                 <img id="Esquerda" src={imgThemes[theme].estrelaEDevlogs} alt="" />
             </div>
+             
+            <div id="SegundaParte">
+               <h3>Mais Devlogs</h3>
+              
+                <div id="MaisDevlogs">
+                     
+                      <div>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+                    <div>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+                    <div>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+                    <div>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+                    <div>
+                        <a href="#">&#62;&#62;More Devlogs bla bla bla bla</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+                    <div>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                        <a href="#">&#62;&#62;More Devlogs</a>
+                    </div>
+
+                </div>
+
+                <img src={imgThemes[theme].estrelaMaisDevlogs} alt="" />
+                
+                
+            </div>
+
+        
+
+            
+
+
         </div>
     )
 }
