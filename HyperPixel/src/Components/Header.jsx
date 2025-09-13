4404ef803ef463d4d749c126515b43fa89da9702
nicole-santana <nicole.teles.santana@gmail.com>
2025-09-13 15:34:30 -0300

    Adição de um botão de tela cheia

--- HyperPixel/src/Components/Header.jsx
+++ HyperPixel/src/Components/Header.jsx
@@ -1,27 +1,34 @@
 import './CSS/main.css'
 import { Link } from 'react-router-dom';
 
-export default function Header({ theme, handleNextTheme }){
+export default function Header({ theme, handleNextTheme }) {
+  // Mova a função para cá:
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  };
 
-    
-    return(
-        <div className="Header">
+  return (
+    <div className="Header">
+      <div className="Pages">
+        <Link to="/"><span>H</span>ome</Link>
+        <Link to="/aboutMe"><span>A</span>bout me</Link>
+        <Link to="/devlogs"><span>D</span>evlogs</Link>
+        <Link to="/otherProjects"><span>O</span>ther projects</Link>
+      </div>
 
-            <div className="Pages">
-                <Link to="/"><span>H</span>ome</Link>
-                <Link to="/aboutMe"><span>A</span>bout me</Link>
-                <Link to="/devlogs"><span>D</span>evlogs</Link>
-                <Link to="/otherProjects"><span>O</span>ther projects</Link>
-        
-                
-            </div>
-            
-                <button onClick={handleNextTheme}>
-                    <span>C</span>hange color theme
-                </button>
-            
-        </div>
-       
-
-    )
+      <div id='OutrasFuncoes'>
+        <button onClick={toggleFullscreen}>
+        ⛶ Tela cheia
+      </button>
+      <button onClick={handleNextTheme}>
+        <span>C</span>hange color theme
+      </button>
+      </div>
+      
+    </div>
+  );
 }
